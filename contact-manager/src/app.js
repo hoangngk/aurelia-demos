7f6157cd4dbe851e5958e8a5a2e44ac9c5e0a396
Hoang Nguyen <n.khanhhoang@gmail.com>
2017-11-29 14:55:30 +0700

    aurelia contact manager - finish app

--- contact-manager/src/app.js
+++ contact-manager/src/app.js
@@ -1,5 +1,21 @@
+import {inject} from 'aurelia-framework';
+import {WebAPI} from "./web-api";
+
+
+@inject(WebAPI)
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+
+  constructor(api) {
+    this.api = api;
+  }
+
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'components/contact-details/contact-details', name:'contacts' }
+    ]);
+
+    this.router = router;
   }
 }
